0f5b9690fd4aa802709042ec89667d63cd671be1
Andrea Rocco Matta <82328931+PlywoodBox@users.noreply.github.com>
2024-08-30 10:34:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import {
+  OrbitControls
+} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'https://cdn.skypack.dev/dat.gui';
+
 const scene = new THREE.Scene();
 
 // Create both cameras
